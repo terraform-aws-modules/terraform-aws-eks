e4263868e82037d78feeed7be95abf89353220d4
Max Williams <max.williams@deliveryhero.com>
2018-07-11 10:43:09 +0200

    terraform fmt fix

--- examples/eks_test_fixture/variables.tf
+++ examples/eks_test_fixture/variables.tf
@@ -5,37 +5,40 @@ variable "region" {
 variable "map_accounts" {
   description = "Additional AWS account numbers to add to the aws-auth configmap."
   type        = "list"
-  default     = [
+
+  default = [
     "777777777777",
-    "888888888888"
+    "888888888888",
   ]
 }
 
 variable "map_roles" {
   description = "Additional IAM roles to add to the aws-auth configmap."
   type        = "list"
-  default     = [
+
+  default = [
     {
       role_arn = "arn:aws:iam::66666666666:role/role1"
       username = "role1"
-      group = "system:masters"
-    }
+      group    = "system:masters"
+    },
   ]
 }
 
 variable "map_users" {
   description = "Additional IAM users to add to the aws-auth configmap."
   type        = "list"
-  default     = [
+
+  default = [
     {
       user_arn = "arn:aws:iam::66666666666:user/user1"
       username = "user1"
-      group  = "system:masters"
+      group    = "system:masters"
     },
     {
       user_arn = "arn:aws:iam::66666666666:user/user2"
       username = "user2"
-      group  = "system:masters"
-    }
+      group    = "system:masters"
+    },
   ]
 }
