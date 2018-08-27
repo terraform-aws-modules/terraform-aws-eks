2e2dd0e215454514d1f97880321857246869a16f
Daniel Piddock <33028589+dpiddockcmp@users.noreply.github.com>
2018-08-27 15:42:17 +0200

    Remove public subnets from eks module call (#106)

--- examples/eks_test_fixture/main.tf
+++ examples/eks_test_fixture/main.tf
@@ -69,7 +69,7 @@ module "vpc" {
 module "eks" {
   source             = "../.."
   cluster_name       = "${local.cluster_name}"
-  subnets            = ["${module.vpc.public_subnets}", "${module.vpc.private_subnets}"]
+  subnets            = ["${module.vpc.private_subnets}"]
   tags               = "${local.tags}"
   vpc_id             = "${module.vpc.vpc_id}"
   worker_groups      = "${local.worker_groups}"
