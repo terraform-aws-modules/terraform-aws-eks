165bcbc2019c7bb943438065648257dde3172e46
Andrew Lavery <laverya@umich.edu>
2018-09-19 17:58:37 -0700

    remove depends_on in worker_role_arns template file
    this was causing resources to be recreated every time

--- aws_auth.tf
+++ aws_auth.tf
@@ -19,7 +19,6 @@ resource "null_resource" "update_config_map_aws_auth" {
 data "aws_caller_identity" "current" {}
 
 data "template_file" "worker_role_arns" {
-  depends_on = ["aws_launch_configuration.workers"]
   count      = "${var.worker_group_count}"
   template   = "${file("${path.module}/templates/worker-role.tpl")}"
 
