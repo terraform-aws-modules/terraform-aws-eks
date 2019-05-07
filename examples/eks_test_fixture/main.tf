c90903e42841b7066da1e570a0d5ebc5fa3a1ada
Max Williams <max.williams@deliveryhero.com>
2019-05-07 15:36:21 +0200

    fixing and simplifying example

--- examples/eks_test_fixture/main.tf
+++ examples/eks_test_fixture/main.tf
@@ -61,24 +61,17 @@ locals {
 
   worker_groups = [
     {
-      # This will launch an autoscaling group with only On-Demand instances
       instance_type        = "t2.small"
       additional_userdata  = "echo foo bar"
-      subnets              = "${join(",", module.vpc.private_subnets)}"
-      asg_desired_capacity = "2"
+      asg_desired_capacity = 2
     },
   ]
   worker_groups_launch_template = [
     {
-      # This will launch an autoscaling group with only Spot Fleet instances
-      instance_type                            = "t2.small"
-      additional_userdata                      = "echo foo bar"
-      subnets                                  = "${join(",", module.vpc.private_subnets)}"
-      additional_security_group_ids            = "${aws_security_group.worker_group_mgmt_one.id},${aws_security_group.worker_group_mgmt_two.id}"
-      override_instance_type                   = "t3.small"
-      asg_desired_capacity                     = "2"
-      spot_instance_pools                      = 10
-      on_demand_percentage_above_base_capacity = "0"
+      instance_type                 = "t2.small"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = "${aws_security_group.worker_group_mgmt_one.id},${aws_security_group.worker_group_mgmt_two.id}"
+      asg_desired_capacity          = 2
     },
   ]
   tags = {
@@ -163,8 +156,8 @@ module "eks" {
   vpc_id                               = "${module.vpc.vpc_id}"
   worker_groups                        = "${local.worker_groups}"
   worker_groups_launch_template        = "${local.worker_groups_launch_template}"
-  worker_group_count                   = "1"
-  worker_group_launch_template_count   = "1"
+  worker_group_count                   = 1
+  worker_group_launch_template_count   = 1
   worker_additional_security_group_ids = ["${aws_security_group.all_worker_mgmt.id}"]
   map_roles                            = "${var.map_roles}"
   map_roles_count                      = "${var.map_roles_count}"
