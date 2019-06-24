a53b39a9eaf57126c7950b7dfb89eefd2004486e
Graham Booth <gdbooth27@gmail.com>
2019-06-24 11:52:58 -0700

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -44,7 +44,7 @@ output "cluster_iam_role_arn" {
 }
 
 output "cloudwatch_log_group_name"{
-  description = ""
+  description = "Name of cloudwatch log group created"
   value       = aws_cloudwatch_log_group.this.name
 }
 
