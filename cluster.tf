ba3377360e13e4773b27b1c5ec3dfd99b8ef949d
till-krauss <40272510+till-krauss@users.noreply.github.com>
2019-06-24 12:53:43 +0200

    Added Option to use KMS key for cloudwatch logs group. (#404)

--- cluster.tf
+++ cluster.tf
@@ -2,6 +2,7 @@ resource "aws_cloudwatch_log_group" "this" {
   count             = length(var.cluster_enabled_log_types) > 0 ? 1 : 0
   name              = "/aws/eks/${var.cluster_name}/cluster"
   retention_in_days = var.cluster_log_retention_in_days
+  kms_key_id        = var.cluster_log_kms_key_id
 }
 
 resource "aws_eks_cluster" "this" {
