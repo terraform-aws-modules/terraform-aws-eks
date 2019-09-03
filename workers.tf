77dfb2cac4b674917e57a6b1a6a5cf787a6cf357
Smuggla <awaskow@evo-m19003.isystems.lan>
2019-09-03 11:31:07 -0400

    Update Workers Group to use cluster_name variable to remove lifecycle dependancy issue

--- workers.tf
+++ workers.tf
@@ -245,14 +245,14 @@ resource "random_pet" "workers" {
 
 resource "aws_security_group" "workers" {
   count       = var.worker_create_security_group ? 1 : 0
-  name_prefix = aws_eks_cluster.this.name
+  name_prefix = var.cluster_name
   description = "Security group for all nodes in the cluster."
   vpc_id      = var.vpc_id
   tags = merge(
     var.tags,
     {
-      "Name"                                               = "${aws_eks_cluster.this.name}-eks_worker_sg"
-      "kubernetes.io/cluster/${aws_eks_cluster.this.name}" = "owned"
+      "Name"                                               = "${var.cluster_name}-eks_worker_sg"
+      "kubernetes.io/cluster/${var.cluster_name}" = "owned"
     },
   )
 }
