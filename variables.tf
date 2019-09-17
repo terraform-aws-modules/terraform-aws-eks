adf15fa83084ca80df4f7896a1146c45f97699eb
Roman Voitenko <roman.voitenko@konsult.atg.se>
2019-09-17 23:31:03 +0200

    added posibility to use Amazon EKS-Optimized AMI with GPU Support for workers

--- variables.tf
+++ variables.tf
@@ -126,6 +126,12 @@ variable "worker_ami_name_filter" {
   default     = "v*"
 }
 
+variable "worker_ami_name_filter_prefix" {
+  description = "Name prefix filter for AWS EKS worker AMI. Default behaviour will get regular EKS-Optimized AMI but could be set to a EKS-Optimized AMI with GPU Support, e.g. \"amazon-eks-gpu-node\", or custom AMI"
+  type        = string
+  default     = "amazon-eks-node"
+}
+
 variable "worker_additional_security_group_ids" {
   description = "A list of additional security group ids to attach to worker instances"
   type        = list(string)
