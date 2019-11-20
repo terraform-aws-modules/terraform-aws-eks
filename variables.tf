954fe92a661c0f9f6014d8c9f33f79a453bad9b0
Tomislav Tomašić <syst0m@users.noreply.github.com>
2019-11-20 12:44:28 +0000

    Update flag variable name

--- variables.tf
+++ variables.tf
@@ -294,7 +294,7 @@ variable "attach_worker_cni_policy" {
   default     = true
 }
 
-variable "create_cluster" {
+variable "create_eks" {
   description = "Controls if EKS resources should be created (it affects almost all resources)"
   type        = bool
   default     = true
