e30360d5bd1d479fce31e39396998a00a1c561c4
Tibo Beijen <tibobeijen@gmail.com>
2019-12-06 16:06:06 +0100

    Revert "Readme and example updates."
    This reverts commit 18a0746355e136010ad54858a1b518406f6a3638.

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -24,19 +24,17 @@ provider "template" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  count = var.create_eks ? 1 : 0
-  name  = module.eks.cluster_id
+  name = module.eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  count = var.create_eks ? 1 : 0
-  name  = module.eks.cluster_id
+  name = module.eks.cluster_id
 }
 
 provider "kubernetes" {
-  host                   = element(concat(data.aws_eks_cluster.cluster[*].endpoint, list("")), 0)
-  cluster_ca_certificate = base64decode(element(concat(data.aws_eks_cluster.cluster[*].certificate_authority.0.data, list("")), 0))
-  token                  = element(concat(data.aws_eks_cluster_auth.cluster[*].token, list("")), 0)
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
   version                = "~> 1.10"
 }
@@ -130,7 +128,6 @@ module "vpc" {
 
 module "eks" {
   source       = "../.."
-  create_eks   = var.create_eks
   cluster_name = local.cluster_name
   subnets      = module.vpc.private_subnets
 
--- examples/launch_templates/variables.tf
+++ examples/launch_templates/variables.tf
@@ -2,7 +2,3 @@ variable "region" {
   default = "us-west-2"
 }
 
-variable "create_eks" {
-  default     = true
-  description = "Set to false to skip creating EKS cluster."
-}
