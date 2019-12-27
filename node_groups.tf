058d26c40741f68ed4db29122b81db5ba4221928
Daniel Piddock <daniel.piddock@teamcmp.com>
2019-12-27 12:54:39 +0100

    Call module from parent

---
+++ node_groups.tf
@@ -0,0 +1,8 @@
+module "node_groups" {
+  source                 = "./modules/node_groups"
+  create_eks             = var.create_eks
+  cluster_name           = var.cluster_name
+  cluster_version        = coalescelist(aws_eks_cluster.this[*].version, [""])[0]
+  workers_group_defaults = local.workers_group_defaults
+  node_groups            = var.node_groups
+}
