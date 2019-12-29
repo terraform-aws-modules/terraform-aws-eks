fa598fd6328bbed093bfb6c57297e8b35fe5cb65
Daniel Piddock <daniel.piddock@teamcmp.com>
2019-12-29 20:56:00 +0100

    Fix double destroy
    Was producing index errors when running destroy on an empty state.

--- modules/node_groups/outputs.tf
+++ modules/node_groups/outputs.tf
@@ -1,9 +1,9 @@
 output "iam_role_arns" {
   description = "IAM role ARNs for EKS node groups. Map, keyed by var.node_groups keys"
-  value       = { for k in local.node_groups_keys : k => aws_eks_node_group.workers[k].node_role_arn }
+  value       = { for k, v in aws_eks_node_group.workers : k => v.node_role_arn }
 }
 
 output "aws_auth_snippet" {
   description = "Snippet for use in aws_auth ConfigMap"
-  value       = distinct([for k in local.node_groups_keys : data.template_file.node_group_arns[k].rendered])
+  value       = distinct([for k, v in data.template_file.node_group_arns : v.rendered])
 }
