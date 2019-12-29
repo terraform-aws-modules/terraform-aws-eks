812daf3fea169b1118b094e8869d949f72817b80
Daniel Piddock <daniel.piddock@teamcmp.com>
2019-12-29 21:54:09 +0100

    Fix index fail if node group manually deleted

--- modules/node_groups/template.tf
+++ modules/node_groups/template.tf
@@ -1,9 +1,9 @@
 data "template_file" "node_group_arns" {
-  for_each = local.node_groups_keys
+  for_each = aws_eks_node_group.workers
   template = file("${path.module}/../../templates/worker-role.tpl")
 
   vars = {
-    worker_role_arn = aws_eks_node_group.workers[each.key].node_role_arn
+    worker_role_arn = each.value.node_role_arn
     platform        = "linux" # Hardcoded because the EKS API currently only supports linux for managed node groups
   }
 }
