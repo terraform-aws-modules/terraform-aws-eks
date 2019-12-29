ada263421e674bafcc2d5d1d9947e866cb29facb
Daniel Piddock <daniel.piddock@teamcmp.com>
2019-12-29 13:11:08 +0100

    Remove node_groups from workers_defaults_defaults

--- modules/node_groups/random.tf
+++ modules/node_groups/random.tf
@@ -10,8 +10,8 @@ resource "random_pet" "node_groups" {
     ec2_ssh_key = lookup(local.node_groups_expanded[each.key], "key_name", var.workers_group_defaults["key_name"])
 
     source_security_group_ids = join("-", compact(
-      lookup(local.node_groups_expanded[each.key], "source_security_group_ids", var.workers_group_defaults["source_security_group_ids"]
-    )))
+      lookup(local.node_groups_expanded[each.key], "source_security_group_ids", [])
+    ))
 
     node_group_name = join("-", [var.cluster_name, each.key])
   }
