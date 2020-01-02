7c8aee7b95eede6af1e68892672b6ec02b92e7ce
Daniel Piddock <daniel.piddock@teamcmp.com>
2020-01-02 16:34:02 +0100

    Hack to have node_groups depend on aws_auth etc
    The AWS Node Groups create or edit the aws-auth ConfigMap so that nodes
    can join the cluster. This breaks the kubernetes resource which cannot
    do a force create. Remove the race condition with explicit depend.
    
    Can't pull the IAM role out of the node_group any more.

--- node_groups.tf
+++ node_groups.tf
@@ -1,7 +1,24 @@
+# Hack to ensure ordering of resource creation. Do not create node_groups
+# before other resources are ready. Removes race conditions
+data "null_data_source" "node_groups" {
+  inputs = {
+    cluster_name = var.cluster_name
+
+    # Ensure these resources are created before "unlocking" the data source.
+    # `depends_on` causes a refresh on every run so is useless here.
+    # [Re]creating or removing these resources will trigger recreation of Node Group resources
+    aws_auth         = coalescelist(kubernetes_config_map.aws_auth[*].id, [""])[0]
+    role_NodePolicy  = coalescelist(aws_iam_role_policy_attachment.workers_AmazonEKSWorkerNodePolicy[*].id, [""])[0]
+    role_CNI_Policy  = coalescelist(aws_iam_role_policy_attachment.workers_AmazonEKS_CNI_Policy[*].id, [""])[0]
+    role_Container   = coalescelist(aws_iam_role_policy_attachment.workers_AmazonEC2ContainerRegistryReadOnly[*].id, [""])[0]
+    role_auotscaling = coalescelist(aws_iam_role_policy_attachment.workers_autoscaling[*].id, [""])[0]
+  }
+}
+
 module "node_groups" {
   source                 = "./modules/node_groups"
   create_eks             = var.create_eks
-  cluster_name           = var.cluster_name
+  cluster_name           = data.null_data_source.node_groups.outputs["cluster_name"]
   cluster_version        = coalescelist(aws_eks_cluster.this[*].version, [""])[0]
   default_iam_role_arn   = coalescelist(aws_iam_role.workers[*].arn, [""])[0]
   workers_group_defaults = local.workers_group_defaults
