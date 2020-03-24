de00694a6323389a42f20240de5b3bf4775059d7
Jaime Hidalgo García <jaimehidalgo2@hotmail.com>
2020-03-24 17:28:59 -0500

    feat: Add support for additional volumes in launch templates and launch configurations (#800)
    Co-authored-by: Jaime Hidalgo <jhidalgo@cccis.com>
    Co-authored-by: Thierno IB. BARRY <ibrahima.br@gmail.com>

--- local.tf
+++ local.tf
@@ -64,6 +64,7 @@ locals {
     service_linked_role_arn       = ""                          # Arn of custom service linked role that Auto Scaling group will use. Useful when you have encrypted EBS
     termination_policies          = []                          # A list of policies to decide how the instances in the auto scale group should be terminated.
     platform                      = "linux"                     # Platform of workers. either "linux" or "windows"
+    additional_ebs_volumes        = []                          # A list of additional volumes to be attached to the instances on this Auto Scaling group. Each volume should be an object with the following: block_device_name (required), volume_size, volume_type, iops, encrypted, kms_key_id (only on launch-template), delete_on_termination. Optional values are grabbed from root volume or from defaults
     # Settings for launch templates
     root_block_device_name            = data.aws_ami.eks_worker.root_device_name # Root device name for workers. If non is provided, will assume default AMI was used.
     root_kms_key_id                   = ""                                       # The KMS key to use when encrypting the root storage device
