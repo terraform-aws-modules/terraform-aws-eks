1e67399a743e9fe3792e7ebd4198065a41b8b5b1
Jianyu Feng <jianyu.feng@paytm.com>
2020-05-01 17:02:46 -0400

    add new var workers_assume_role_services to  the example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -156,4 +156,5 @@ module "eks" {
   map_roles                            = var.map_roles
   map_users                            = var.map_users
   map_accounts                         = var.map_accounts
+  workers_assume_role_services         = []
 }
