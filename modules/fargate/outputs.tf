3d348be7c5ce5ceea172f22e0690365dc62a8a02
Simon Gurcke <simon@gurcke.de>
2020-05-27 13:25:51 +1000

    Simplify aws_auth_roles output

--- modules/fargate/outputs.tf
+++ modules/fargate/outputs.tf
@@ -11,8 +11,8 @@ output "iam_role_arn" {
 output "aws_auth_roles" {
   description = "Roles for use in aws-auth ConfigMap"
   value = [
-    for index in range(0, var.create_eks ? 1 : 0) : {
-      worker_role_arn = element(concat(aws_iam_role.eks_fargate_pod.*.arn, list("")), 0)
+    for role in aws_iam_role.eks_fargate_pod : {
+      worker_role_arn = role.arn
       platform        = "fargate"
     }
   ]
