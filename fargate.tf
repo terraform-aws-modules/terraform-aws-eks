b8af582980065422ac18b93e66f4bfd55ff90772
Simon Gurcke <simon@gurcke.de>
2020-05-27 13:24:57 +1000

    Remove unnecessary variables

--- fargate.tf
+++ fargate.tf
@@ -5,6 +5,4 @@ module "fargate" {
   fargate_profiles                  = var.fargate_profiles
   subnets                           = var.subnets
   tags                              = var.tags
-  cluster_primary_security_group_id = element(concat(aws_eks_cluster.this[*].vpc_config[0].cluster_security_group_id, list("")), 0)
-  worker_security_group_id          = local.worker_security_group_id
 }
--- modules/fargate/variables.tf
+++ modules/fargate/variables.tf
@@ -27,13 +27,3 @@ variable "tags" {
   description = "A map of tags to add to all resources."
   type        = map(string)
 }
-
-variable "cluster_primary_security_group_id" {
-  description = "Cluster primary security group ID created by the EKS cluster on 1.14 or later. Referred to as 'Cluster security group' in the EKS console."
-  type        = string
-}
-
-variable "worker_security_group_id" {
-  description = "Security group ID attached to the EKS workers."
-  type        = string
-}
