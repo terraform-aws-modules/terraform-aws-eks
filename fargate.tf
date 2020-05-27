13034069a47c8061429120f50320f3502b011f00
Simon Gurcke <simon@gurcke.de>
2020-05-27 13:31:43 +1000

    Fix formatting

--- fargate.tf
+++ fargate.tf
@@ -1,8 +1,8 @@
 module "fargate" {
-  source                            = "./modules/fargate"
-  cluster_name                      = coalescelist(aws_eks_cluster.this[*].name, [""])[0]
-  create_eks                        = var.create_eks
-  fargate_profiles                  = var.fargate_profiles
-  subnets                           = var.subnets
-  tags                              = var.tags
+  source           = "./modules/fargate"
+  cluster_name     = coalescelist(aws_eks_cluster.this[*].name, [""])[0]
+  create_eks       = var.create_eks
+  fargate_profiles = var.fargate_profiles
+  subnets          = var.subnets
+  tags             = var.tags
 }
