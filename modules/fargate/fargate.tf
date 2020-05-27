e4c0d4924b460c1e07f64b41a736cf71b61daded
Simon Gurcke <simon@gurcke.de>
2020-05-27 13:22:43 +1000

    Update modules/fargate/fargate.tf
    Co-authored-by: Daniel Piddock <33028589+dpiddockcmp@users.noreply.github.com>

--- modules/fargate/fargate.tf
+++ modules/fargate/fargate.tf
@@ -33,7 +33,7 @@ resource "aws_eks_fargate_profile" "this" {
   for_each               = var.create_eks ? var.fargate_profiles : {}
   cluster_name           = var.cluster_name
   fargate_profile_name   = lookup(each.value, "name", format("%s-fargate-%s", var.cluster_name, replace(each.key, "_", "-")))
-  pod_execution_role_arn = join("", aws_iam_role.eks_fargate_pod.*.arn)
+  pod_execution_role_arn = aws_iam_role.eks_fargate_pod[0].arn
   subnet_ids             = var.subnets
   tags                   = var.tags
 
