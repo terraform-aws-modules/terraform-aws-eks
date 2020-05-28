7199e015be10ef42e0ca67619ba7fa32a505a8ec
Simon Gurcke <simon@gurcke.de>
2020-05-28 16:25:05 +1000

    Add create_fargate_pod_execution_role input

--- fargate.tf
+++ fargate.tf
@@ -1,8 +1,9 @@
 module "fargate" {
-  source           = "./modules/fargate"
-  cluster_name     = coalescelist(aws_eks_cluster.this[*].name, [""])[0]
-  create_eks       = var.create_eks
-  fargate_profiles = var.fargate_profiles
-  subnets          = var.subnets
-  tags             = var.tags
+  source                            = "./modules/fargate"
+  cluster_name                      = coalescelist(aws_eks_cluster.this[*].name, [""])[0]
+  create_eks                        = var.create_eks
+  create_fargate_pod_execution_role = var.create_fargate_pod_execution_role
+  fargate_profiles                  = var.fargate_profiles
+  subnets                           = var.subnets
+  tags                              = var.tags
 }
--- modules/fargate/fargate.tf
+++ modules/fargate/fargate.tf
@@ -1,7 +1,7 @@
 # EKS Fargate Pod Execution Role
 
 data "aws_iam_policy_document" "eks_fargate_pod_assume_role" {
-  count = var.create_eks ? 1 : 0
+  count = var.create_eks && var.create_fargate_pod_execution_role ? 1 : 0
   statement {
     effect  = "Allow"
     actions = ["sts:AssumeRole"]
@@ -14,14 +14,14 @@ data "aws_iam_policy_document" "eks_fargate_pod_assume_role" {
 }
 
 resource "aws_iam_role" "eks_fargate_pod" {
-  count              = var.create_eks ? 1 : 0
+  count              = var.create_eks && var.create_fargate_pod_execution_role ? 1 : 0
   name               = format("%s-fargate", var.cluster_name)
   assume_role_policy = data.aws_iam_policy_document.eks_fargate_pod_assume_role[0].json
   tags               = var.tags
 }
 
 resource "aws_iam_role_policy_attachment" "eks_fargate_pod" {
-  count      = var.create_eks ? 1 : 0
+  count      = var.create_eks && var.create_fargate_pod_execution_role ? 1 : 0
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
   role       = aws_iam_role.eks_fargate_pod[0].name
 }
