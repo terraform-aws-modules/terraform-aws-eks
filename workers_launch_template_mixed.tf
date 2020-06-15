terraform-aws-modules/terraform-aws-eks

commit c660b6edeb8fbaf2cf0d27e4ed6c6e954e155007

improvement: Add cpu_credits for tf0.11 (#869)
* Add cpu_credits to tf0.11

* Fix links in changelog

* Set latest tf0.11 version for tests

workers_launch_template_mixed.tf
@@ -88,6 +88,10 @@ resource "aws_launch_template" "workers_launch_template_mixed" {
   user_data     = "${base64encode(element(data.template_file.workers_launch_template_mixed.*.rendered, count.index))}"
   ebs_optimized = "${lookup(var.worker_groups_launch_template_mixed[count.index], "ebs_optimized", lookup(local.ebs_optimized, lookup(var.worker_groups_launch_template_mixed[count.index], "instance_type", local.workers_group_defaults["instance_type"]), false))}"
 
+  credit_specification {
+    cpu_credits = "${lookup(var.worker_groups_launch_template[count.index], "cpu_credits", local.workers_group_defaults["cpu_credits"])}"
+  }
+
   monitoring {
     enabled = "${lookup(var.worker_groups_launch_template_mixed[count.index], "enable_monitoring", local.workers_group_defaults["enable_monitoring"])}"
   }

.travis.yml
@@ -23,7 +23,7 @@ install:
 - bundle install
 
 before_script:
-- export TERRAFORM_VERSION=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')
+- export TERRAFORM_VERSION='0.11.14'
 - curl --silent --output terraform.zip "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
 - unzip terraform.zip ; rm -f terraform.zip; chmod +x terraform
 - mkdir -p ${HOME}/bin ; export PATH=${PATH}:${HOME}/bin; mv terraform ${HOME}/bin/

local.tf
@@ -54,6 +54,7 @@ locals {
     launch_template_placement_group   = ""                                            # The name of the placement group into which to launch the instances, if any.
     root_encrypted                    = ""                                            # Whether the volume should be encrypted or not
     eni_delete                        = true                                          # Delete the ENI on termination (if set to false you will have to manually delete before destroying)
+    cpu_credits                       = "standard"                                    # T2/T3 unlimited mode, can be 'standard' or 'unlimited'. Used 'standard' mode as default to avoid paying higher costs
 
     # Settings for launch templates with mixed instances policy
     override_instance_type_1                 = "m5.large"     # Override instance type 1 for mixed instances policy

workers_launch_template.tf
@@ -64,6 +64,10 @@ resource "aws_launch_template" "workers_launch_template" {
   user_data     = "${base64encode(element(data.template_file.launch_template_userdata.*.rendered, count.index))}"
   ebs_optimized = "${lookup(var.worker_groups_launch_template[count.index], "ebs_optimized", lookup(local.ebs_optimized, lookup(var.worker_groups_launch_template[count.index], "instance_type", local.workers_group_defaults["instance_type"]), false))}"
 
+  credit_specification {
+    cpu_credits = "${lookup(var.worker_groups_launch_template[count.index], "cpu_credits", local.workers_group_defaults["cpu_credits"])}"
+  }
+
   monitoring {
     enabled = "${lookup(var.worker_groups_launch_template[count.index], "enable_monitoring", local.workers_group_defaults["enable_monitoring"])}"
   }

commit e9e9312d0a15821f3f25b0927565d9bf059f012f

Fix cpu_credits worker group lookup variable.
Fixes a lookup against the wrong worker group list
introduced by commit c660b6e to add support to set
cpu_credits.

workers_launch_template_mixed.tf
@@ -90,7 +90,7 @@ resource "aws_launch_template" "workers_launch_template_mixed" {
   ebs_optimized = "${lookup(var.worker_groups_launch_template_mixed[count.index], "ebs_optimized", lookup(local.ebs_optimized, lookup(var.worker_groups_launch_template_mixed[count.index], "instance_type", local.workers_group_defaults["instance_type"]), false))}"
 
   credit_specification {
-    cpu_credits = "${lookup(var.worker_groups_launch_template[count.index], "cpu_credits", local.workers_group_defaults["cpu_credits"])}"
+    cpu_credits = "${lookup(var.worker_groups_launch_template_mixed[count.index], "cpu_credits", local.workers_group_defaults["cpu_credits"])}"
   }
 
   monitoring {