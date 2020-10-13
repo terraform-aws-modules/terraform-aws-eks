ecd5e8ab4401842ed61876c72efe2a2ea4cf3147
Alex Hodges <ahodges22@gmail.com>
2020-10-13 02:06:22 -0700

    feat: upgrade to tf13

--- versions.tf
+++ versions.tf
@@ -1,4 +1,18 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
 }
