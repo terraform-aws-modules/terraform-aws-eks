0b0d56d4608ea5e27f9193400eb5979ffd5a06ce
Billy Howard <billy@osg.me>
2020-11-12 08:25:07 +0000

    refactor how we fix authmap with empty arn for fargate.

--- modules/fargate/outputs.tf
+++ modules/fargate/outputs.tf
@@ -24,6 +24,6 @@ output "aws_auth_roles" {
     for i in range(1) : {
       worker_role_arn = local.pod_execution_role_arn
       platform        = "fargate"
-    } if length(var.fargate_profiles) > 0
+    } if local.create_eks
   ]
 }
