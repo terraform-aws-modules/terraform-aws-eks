e2ce91d98726ecd21eb7a95abb75403c895ea4ef
Oscar Utbult <oscar.utbult@gmail.com>
2020-12-31 11:05:42 +0100

    fix: Change example instance type from t2 to t3.
    The t2 instance type isn't available in newer regions such as
    eu-north-1. Therefore the examples using t2 will fail in this region.

--- examples/secrets_encryption/main.tf
+++ examples/secrets_encryption/main.tf
@@ -103,7 +103,7 @@ module "eks" {
   worker_groups = [
     {
       name                 = "worker-group-1"
-      instance_type        = "t2.small"
+      instance_type        = "t3.small"
       additional_userdata  = "echo foo bar"
       asg_desired_capacity = 2
     },
