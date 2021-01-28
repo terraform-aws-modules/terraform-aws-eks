4fc609bd22459fce7863872ac032d9e89d75009f
Sean P. Kane <spkane00@gmail.com>
2021-01-28 14:45:41 -0800

    feat: Add nitro enclave support for EKS (#1185)

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12.9, != 0.13.0"
 
   required_providers {
-    aws        = ">= 3.21.0"
+    aws        = ">= 3.22.0"
     local      = ">= 1.4"
     null       = ">= 2.1"
     template   = ">= 2.1"
