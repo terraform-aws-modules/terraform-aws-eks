89202016f522472f77d35d935ad7474bf4147494
Jerry Aldrich <jerry@smartcontract.com>
2021-01-27 22:39:58 -0800

    docs: cluster_endpoint_private_access_cidrs
    This clarifies that `cluster_create_endpoint_private_access_sg_rule`
    needs to be set to `true` to use `cluster_endpoint_private_access_cidrs`

--- variables.tf
+++ variables.tf
@@ -260,7 +260,7 @@ variable "cluster_create_endpoint_private_access_sg_rule" {
 }
 
 variable "cluster_endpoint_private_access_cidrs" {
-  description = "List of CIDR blocks which can access the Amazon EKS private API server endpoint."
+  description = "List of CIDR blocks which can access the Amazon EKS private API server endpoint. To use this `cluster_create_endpoint_private_access_sg_rule` must be set to `true`"
   type        = list(string)
   default     = null
 }
