3316c8272995c179f4c7628c313d90abed1f2b75
FERNANDEZ Xavier (Canal Plus Prestataire) <xavier.fernandez@cpexterne.org>
2021-01-28 11:42:15 +0100

    feat: allow to specify the managed cluster IAM role name

--- cluster.tf
+++ cluster.tf
@@ -118,7 +118,8 @@ resource "aws_security_group_rule" "cluster_https_worker_ingress" {
 
 resource "aws_iam_role" "cluster" {
   count                 = var.manage_cluster_iam_resources && var.create_eks ? 1 : 0
-  name_prefix           = var.cluster_name
+  name_prefix           = var.cluster_iam_role_name != "" ? null : var.cluster_name
+  name                  = var.cluster_iam_role_name != "" ? var.cluster_iam_role_name : null
   assume_role_policy    = data.aws_iam_policy_document.cluster_assume_role_policy.json
   permissions_boundary  = var.permissions_boundary
   path                  = var.iam_path
