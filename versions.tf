658813616c6edfba8ba27bb016b0f3ae7f11e43d
Dov Salomon <dov@yieldx.app>
2021-04-12 18:02:17 -0400

    Remove dependency on deprecated hashicorp/template
    hashicorp/template is deprecated, and the templatefile function should be used instead.

--- versions.tf
+++ versions.tf
@@ -5,7 +5,6 @@ terraform {
     aws        = ">= 3.22.0"
     local      = ">= 1.4"
     null       = ">= 2.1"
-    template   = ">= 2.1"
     random     = ">= 2.1"
     kubernetes = ">= 1.11.1"
   }
