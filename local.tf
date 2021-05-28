5d46764bdbfc15d8a2cec639faec71de97a01285
Thierno IB. BARRY <ibrahima.br@gmail.com>
2021-05-28 08:36:28 +0200

    Update local.tf

--- local.tf
+++ local.tf
@@ -78,7 +78,7 @@ locals {
     warm_pool                         = null                        # If this block is configured, add a Warm Pool to the specified Auto Scaling group.
 
     # Settings for launch templates
-    root_block_device_name               = concat(data.aws_ami.eks_worker.*.root_device_name, [""])[0]         # Root device name for workers. If non is provided, will assume default AMI was used.
+    root_block_device_name               = concat(data.aws_ami.eks_worker.*.root_device_name, [""])[0]         # Root device name for Linux workers. If not provided, will assume default Linux AMI was used.
     root_block_device_name_windows       = concat(data.aws_ami.eks_worker_windows.*.root_device_name, [""])[0] # Root device name for Windows workers. If non is provided, will assume default AMI was used.
     root_kms_key_id                      = ""                                                                  # The KMS key to use when encrypting the root storage device
     launch_template_id                   = null                                                                # The id of the launch template used for managed node_groups
