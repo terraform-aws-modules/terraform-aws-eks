8c84f4078f68476f37bdc05207c16ef1ab317049
James Callahan <jamescallahan@bitgo.com>
2021-06-07 15:52:41 +1000

    feat: create the fargate role even if number of fargate profiles is 0
    Set `create_fargate_pod_execution_role` to `false` to opt out.

--- modules/fargate/locals.tf
+++ modules/fargate/locals.tf
@@ -1,7 +1,6 @@
 locals {
-  create_eks              = var.create_eks && length(var.fargate_profiles) > 0
-  pod_execution_role_arn  = var.create_fargate_pod_execution_role ? element(concat(aws_iam_role.eks_fargate_pod.*.arn, [""]), 0) : element(concat(data.aws_iam_role.custom_fargate_iam_role.*.arn, [""]), 0)
-  pod_execution_role_name = var.create_fargate_pod_execution_role ? element(concat(aws_iam_role.eks_fargate_pod.*.name, [""]), 0) : element(concat(data.aws_iam_role.custom_fargate_iam_role.*.name, [""]), 0)
+  pod_execution_role_arn  = concat(aws_iam_role.eks_fargate_pod.*.arn, data.aws_iam_role.custom_fargate_iam_role.*.arn)[0]
+  pod_execution_role_name = concat(aws_iam_role.eks_fargate_pod.*.name, data.aws_iam_role.custom_fargate_iam_role.*.name)[0]
 
   fargate_profiles_expanded = { for k, v in var.fargate_profiles : k => merge(
     v,
