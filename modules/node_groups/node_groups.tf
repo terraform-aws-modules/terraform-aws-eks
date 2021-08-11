5abf3dc926973195c8b5ba6e4e648a6f84395fa2
Justin Ross <justin.ross@betterworks.com>
2021-08-11 17:24:55 -0600

    Adding dependency on launch templation creation to node group creation

--- modules/node_groups/node_groups.tf
+++ modules/node_groups/node_groups.tf
@@ -1,86 +1,86 @@
-resource "aws_eks_node_group" "workers" {
-  for_each = local.node_groups_expanded
+# resource "aws_eks_node_group" "workers" {
+#   for_each = local.node_groups_expanded
 
-  node_group_name = each.value["name"]
-  version         = lookup(each.value, "version", null)
-  capacity_type   = each.value["capacity_type"] # SPOT or ON_DEMAND
+#   node_group_name = each.value["name"]
+#   version         = lookup(each.value, "version", null)
+#   capacity_type   = each.value["capacity_type"] # SPOT or ON_DEMAND
 
-  cluster_name  = var.cluster_name
-  node_role_arn = each.value["iam_role_arn"]
-  subnet_ids    = each.value["subnets"]
+#   cluster_name  = var.cluster_name
+#   node_role_arn = each.value["iam_role_arn"]
+#   subnet_ids    = each.value["subnets"]
 
-  scaling_config {
-    desired_size = each.value["desired_capacity"]
-    max_size     = each.value["max_capacity"]
-    min_size     = each.value["min_capacity"]
-  }
+#   scaling_config {
+#     desired_size = each.value["desired_capacity"]
+#     max_size     = each.value["max_capacity"]
+#     min_size     = each.value["min_capacity"]
+#   }
 
-  disk_size       = var.node_groups_defaults["disk_size"]
-  instance_types  = each.value["instance_types"]
+#   disk_size       = var.node_groups_defaults["disk_size"]
+#   instance_types  = each.value["instance_types"]
 
-  # These shouldn't be needed as we specify the version
-  ami_type        = lookup(each.value, "ami_type", null)
-  release_version = lookup(each.value, "ami_release_version", null)
+#   # These shouldn't be needed as we specify the version
+#   ami_type        = lookup(each.value, "ami_type", null)
+#   release_version = lookup(each.value, "ami_release_version", null)
 
-  dynamic "remote_access" {
-    for_each = each.value["key_name"] != "" ? [{
-      ec2_ssh_key               = each.value["key_name"]
-      source_security_group_ids = lookup(each.value, "source_security_group_ids", [])
-    }] : []
+#   dynamic "remote_access" {
+#     for_each = each.value["key_name"] != "" ? [{
+#       ec2_ssh_key               = each.value["key_name"]
+#       source_security_group_ids = lookup(each.value, "source_security_group_ids", [])
+#     }] : []
 
-    content {
-      ec2_ssh_key               = remote_access.value["ec2_ssh_key"]
-      source_security_group_ids = remote_access.value["source_security_group_ids"]
-    }
-  }
+#     content {
+#       ec2_ssh_key               = remote_access.value["ec2_ssh_key"]
+#       source_security_group_ids = remote_access.value["source_security_group_ids"]
+#     }
+#   }
 
-  # launch_template = aws_launch_configuration.workers.*.id[count.index]
+#   # launch_template = aws_launch_configuration.workers.*.id[count.index]
 
-  dynamic "launch_template" {
-    for_each = each.value["launch_template_id"] != null ? [{
-      id      = each.value["launch_template_id"]
-      version = each.value["launch_template_version"]
-    }] : []
+#   dynamic "launch_template" {
+#     for_each = each.value["launch_template_id"] != null ? [{
+#       id      = each.value["launch_template_id"]
+#       version = each.value["launch_template_version"]
+#     }] : []
 
-    content {
-      id      = launch_template.value["id"]
-      version = launch_template.value["version"]
-    }
-  }
+#     content {
+#       id      = launch_template.value["id"]
+#       version = launch_template.value["version"]
+#     }
+#   }
 
-  dynamic "launch_template" {
-    for_each = each.value["launch_template_id"] == null && each.value["create_launch_template"] ? [{
-      id      = aws_launch_template.workers[each.key].id
-      version = each.value["launch_template_version"]
-    }] : []
+#   dynamic "launch_template" {
+#     for_each = each.value["launch_template_id"] == null && each.value["create_launch_template"] ? [{
+#       id      = aws_launch_template.workers[each.key].id
+#       version = each.value["launch_template_version"]
+#     }] : []
 
-    content {
-      id      = launch_template.value["id"]
-      version = launch_template.value["version"]
-    }
-  }
+#     content {
+#       id      = launch_template.value["id"]
+#       version = launch_template.value["version"]
+#     }
+#   }
 
 
 
-  labels = merge(
-    lookup(var.node_groups_defaults, "k8s_labels", {}),
-    lookup(var.node_groups[each.key], "k8s_labels", {})
-  )
+#   labels = merge(
+#     lookup(var.node_groups_defaults, "k8s_labels", {}),
+#     lookup(var.node_groups[each.key], "k8s_labels", {})
+#   )
 
-  tags = merge(
-    {
-      Name = "${each.value["name"]}_node"
-    },
-    var.tags,
-    lookup(var.node_groups_defaults, "additional_tags", {}),
-    lookup(var.node_groups[each.key], "additional_tags", {}),
-  )
+#   tags = merge(
+#     {
+#       Name = "${each.value["name"]}_node"
+#     },
+#     var.tags,
+#     lookup(var.node_groups_defaults, "additional_tags", {}),
+#     lookup(var.node_groups[each.key], "additional_tags", {}),
+#   )
 
-  lifecycle {
-    create_before_destroy = true
-    ignore_changes        = [scaling_config.0.desired_size]
-  }
+#   lifecycle {
+#     create_before_destroy = true
+#     ignore_changes        = [scaling_config.0.desired_size]
+#   }
 
-  depends_on = [aws_launch_template.workers]
+#   depends_on = [aws_launch_template.workers]
 
-}
+# }
