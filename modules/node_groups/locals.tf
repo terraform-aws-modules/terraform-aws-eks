81cbd01dad8a513b498fa983805c1480da0b5f27
Justin Ross <justin.ross@betterworks.com>
2021-08-11 16:14:02 -0600

    Adding dependency on launch templation creation to node group creation

--- modules/node_groups/locals.tf
+++ modules/node_groups/locals.tf
@@ -56,5 +56,5 @@ locals {
   ) }
 
   # Hack for dependency
-  ng_dependency = local.node_groups_defaults["launch_template_id"] == null && local.node_groups_defaults["create_launch_template"] ? [aws_launch_template.workers] : []
+  ng_dependency = local.node_groups_defaults["launch_template_id"] == null && local.node_groups_defaults["create_launch_template"] ? aws_launch_template.workers : ""
 }
--- modules/node_groups/node_groups.tf
+++ modules/node_groups/node_groups.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_node_group" "workers" {
-  depends_on = local.ng_dependency
+  depends_on = [local.ng_dependency]
   for_each = local.node_groups_expanded
 
   node_group_name = each.value["name"]
