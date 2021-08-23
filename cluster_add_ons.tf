4f1d264644c02189bae75759dff8bed06ae3feca
Justin Ross <justin.ross@betterworks.com>
2021-08-23 10:55:19 -0600

    Fixing cluster name

--- cluster_add_ons.tf
+++ cluster_add_ons.tf
@@ -2,7 +2,7 @@
 resource "aws_eks_addon" "vpc_cni" {
   # while we're upgrading we don't want these to be created until we're on 1.20 cluster
   count = var.create_eks && var.enable_vpc_cni_addon ? 1 : 0
-  cluster_name = aws_eks_cluster.this.*.name
+  cluster_name = aws_eks_cluster.this[0].name
   addon_name   = "vpc-cni"
   addon_version = var.vpc_cni_version
   resolve_conflicts = var.vpc_cni_resolve_conflicts
@@ -10,7 +10,7 @@ resource "aws_eks_addon" "vpc_cni" {
 
 resource "aws_eks_addon" "coredns" {
   count = var.create_eks && var.enable_coredns_addon ? 1 : 0
-  cluster_name = aws_eks_cluster.this.*.name
+  cluster_name = aws_eks_cluster.this[0].name
   addon_name   = "coredns"
   addon_version = var.coredns_version
   resolve_conflicts = var.coredns_resolve_conflicts
@@ -18,7 +18,7 @@ resource "aws_eks_addon" "coredns" {
 
 resource "aws_eks_addon" "kube_proxy" {
   count = var.create_eks && var.enable_kube_proxy_addon ? 1 : 0
-  cluster_name = aws_eks_cluster.this.*.name
+  cluster_name = aws_eks_cluster.this[0].name
   addon_name   = "kube-proxy"
   addon_version = var.kube_proxy_version
   resolve_conflicts = var.kube_proxy_resolve_conflicts
