0f92cec1a1ce875fa84f2556b79518082f37ce4e
Anton Babenko <anton@antonbabenko.com>
2021-08-31 15:07:48 +0200

    Updated example a bit

--- examples/launch_templates/main.tf
+++ examples/launch_templates/main.tf
@@ -1,21 +1,21 @@
 provider "aws" {
   region = var.region
 }
-#
-#data "aws_eks_cluster" "cluster" {
-#  name = module.eks.cluster_id
-#}
-#
-#data "aws_eks_cluster_auth" "cluster" {
-#  name = module.eks.cluster_id
-#}
-#
-#provider "kubernetes" {
-#  host                   = data.aws_eks_cluster.cluster.endpoint
-#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-#  token                  = data.aws_eks_cluster_auth.cluster.token
-#  load_config_file       = false
-#}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
 
 data "aws_availability_zones" "available" {
 }
