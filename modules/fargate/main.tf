b248cce7c7a7a82d8aa6d9a762259ff61216f220
Emanuel Oliveira <emanuelolive@grupoboticario.com.br>
2021-09-17 14:34:06 -0300

    feat: define fargate pod execution role name
    Set a custom name to "fargate pod execution role" if variable `var.fargate_pod_execution_role_name` is defined.

--- modules/fargate/main.tf
+++ modules/fargate/main.tf
@@ -32,7 +32,8 @@ data "aws_iam_role" "custom_fargate_iam_role" {
 resource "aws_iam_role" "eks_fargate_pod" {
   count = local.create_eks && var.create_fargate_pod_execution_role ? 1 : 0
 
-  name_prefix          = format("%s-fargate", substr(var.cluster_name, 0, 24))
+  name_prefix          = var.fargate_pod_execution_role_name != "" ? null : format("%s-fargate", substr(var.cluster_name, 0, 24))
+  name                 = var.fargate_pod_execution_role_name != "" ? var.fargate_pod_execution_role_name : null
   assume_role_policy   = data.aws_iam_policy_document.eks_fargate_pod_assume_role[0].json
   permissions_boundary = var.permissions_boundary
   tags                 = var.tags
