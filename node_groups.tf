d7a092502a8bbe2e8e7df2813f3f6ed7bb7a5efa
Bryant Biggs <bryantbiggs@gmail.com>
2022-01-14 15:13:29 -0500

    chore: set instance metadata tags on by default

--- node_groups.tf
+++ node_groups.tf
@@ -3,6 +3,7 @@ locals {
     http_endpoint               = "enabled"
     http_tokens                 = "required"
     http_put_response_hop_limit = 2
+    instance_metadata_tags      = "enabled"
   }
 }
 
