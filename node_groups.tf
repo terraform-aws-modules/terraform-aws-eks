303aae0f1a79cd04c3415fdacb80a870173ea910
Bryant Biggs <bryantbiggs@gmail.com>
2022-01-18 08:56:30 -0500

    fix: Change `instance_metadata_tags` to default to `null`/`disabled` due to tag key pattern conflict

--- node_groups.tf
+++ node_groups.tf
@@ -3,7 +3,6 @@ locals {
     http_endpoint               = "enabled"
     http_tokens                 = "required"
     http_put_response_hop_limit = 2
-    instance_metadata_tags      = "enabled"
   }
 }
 
