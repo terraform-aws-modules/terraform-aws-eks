af68ee9a31f36a87df47287e34b846ee3134de5d
kahirokunn <okinakahiro@gmail.com>
2022-02-03 21:35:09 +0900

    change launch_template_tag_specifications_tags variable to launch_template_tags

--- modules/eks-managed-node-group/main.tf
+++ modules/eks-managed-node-group/main.tf
@@ -232,7 +232,7 @@ resource "aws_launch_template" "this" {
     for_each = toset(["instance", "volume", "network-interface"])
     content {
       resource_type = tag_specifications.key
-      tags          = merge(var.tags, { Name = var.name }, var.launch_template_tag_specifications_tags)
+      tags          = merge(var.tags, { Name = var.name }, var.launch_template_tags)
     }
   }
 
