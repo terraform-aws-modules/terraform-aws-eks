9af0c2495a1fe7a02411ac436f48f6d9ca8b359f
Bryant Biggs <bryantbiggs@gmail.com>
2022-03-02 12:26:20 -0500

    fix: Add support for overriding DNS suffix for cluster IAM role service principal endpoint (#1905)

--- variables.tf
+++ variables.tf
@@ -305,6 +305,14 @@ variable "iam_role_additional_policies" {
   default     = []
 }
 
+# TODO - hopefully this can be removed once the AWS endpoint is named properly in China
+# https://github.com/terraform-aws-modules/terraform-aws-eks/issues/1904
+variable "cluster_iam_role_dns_suffix" {
+  description = "Base DNS domain name for the current partition (e.g., amazonaws.com in AWS Commercial, amazonaws.com.cn in AWS China)"
+  type        = string
+  default     = null
+}
+
 variable "iam_role_tags" {
   description = "A map of additional tags to add to the IAM role created"
   type        = map(string)
