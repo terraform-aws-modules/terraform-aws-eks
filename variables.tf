f2dda4002bab0bb0e852bf567a76673ee2d08e73
Bryant Biggs <bryantbiggs@gmail.com>
2022-04-21 11:32:04 -0400

    chore: update variable name for enabling sg tags

--- variables.tf
+++ variables.tf
@@ -101,7 +101,7 @@ variable "cluster_tags" {
   default     = {}
 }
 
-variable "tag_cluster_primary_security_group" {
+variable "create_cluster_primary_security_group_tags" {
   description = "Indicates whether or not to tag the cluster's primary security group. This security group is created by the EKS service, not the module, and therefore tagging is handled after cluster creation"
   type        = bool
   default     = true
