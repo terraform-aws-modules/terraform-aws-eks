af8b9929966343e4386eff5ec62602bcd894989b
Gabriel Féron <g@leirbag.net>
2022-05-23 09:42:35 +0200

    Use variable in node_groups.tf

--- node_groups.tf
+++ node_groups.tf
@@ -389,6 +389,7 @@ module "self_managed_node_group" {
 
   delete_timeout   = try(each.value.delete_timeout, var.self_managed_node_group_defaults.delete_timeout, null)
   use_default_tags = try(each.value.use_default_tags, var.self_managed_node_group_defaults.use_default_tags, false)
+  autoscaling_group_tags = try(each.value.autoscaling_group_tags, var.self_managed_node_group_defaults.autoscaling_group_tags, {})
 
   # User data
   platform                 = try(each.value.platform, var.self_managed_node_group_defaults.platform, "linux")
