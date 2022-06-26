6b1c790c66889e68c97913a228cfeb1930ef0537
Bryant Biggs <bryantbiggs@gmail.com>
2022-06-26 10:05:51 -0400

    feat: Add support for creating KMS key for cluster secret encryption

--- examples/eks_managed_node_group/outputs.tf
+++ examples/eks_managed_node_group/outputs.tf
@@ -43,6 +43,25 @@ output "cluster_primary_security_group_id" {
 }
 
 ################################################################################
+# KMS Key
+################################################################################
+
+output "key_arn" {
+  description = "The Amazon Resource Name (ARN) of the key"
+  value       = module.eks.key_arn
+}
+
+output "key_id" {
+  description = "The globally unique identifier for the key"
+  value       = module.eks.key_id
+}
+
+output "key_policy" {
+  description = "The IAM resource policy set on the key"
+  value       = module.eks.key_policy
+}
+
+################################################################################
 # Security Group
 ################################################################################
 
