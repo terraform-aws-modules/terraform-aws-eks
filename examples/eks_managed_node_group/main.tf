terraform-aws-modules/terraform-aws-eks

commit 4543ab454bea80b64381b88a631d955a7cfae247

feat: Add support for specifiying NTP address to use private Amazon Time Sync Service (#2125)

examples/eks_managed_node_group/main.tf
@@ -92,6 +92,7 @@ module "eks" {
   }
 
   # Extend node-to-node security group rules
+  node_security_group_ntp_ipv4_cidr_block = ["fd00:ec2::123/128"]
   node_security_group_additional_rules = {
     ingress_self_all = {
       description = "Node to node all ports/protocols"

examples/complete/main.tf
@@ -77,6 +77,7 @@ module "eks" {
   }
 
   # Extend node-to-node security group rules
+  node_security_group_ntp_ipv4_cidr_block = ["169.254.169.123/32"]
   node_security_group_additional_rules = {
     ingress_self_all = {
       description = "Node to node all ports/protocols"

node_groups.tf
@@ -130,8 +130,8 @@ locals {
       from_port        = 123
       to_port          = 123
       type             = "egress"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = var.cluster_ip_family == "ipv6" ? ["::/0"] : null
+      cidr_blocks      = var.node_security_group_ntp_ipv4_cidr_block
+      ipv6_cidr_blocks = var.cluster_ip_family == "ipv6" ? var.node_security_group_ntp_ipv6_cidr_block : null
     }
     egress_ntp_udp = {
       description      = "Egress NTP/UDP to internet"
@@ -139,8 +139,8 @@ locals {
       from_port        = 123
       to_port          = 123
       type             = "egress"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = var.cluster_ip_family == "ipv6" ? ["::/0"] : null
+      cidr_blocks      = var.node_security_group_ntp_ipv4_cidr_block
+      ipv6_cidr_blocks = var.cluster_ip_family == "ipv6" ? var.node_security_group_ntp_ipv6_cidr_block : null
     }
   }
 }

variables.tf
@@ -322,6 +322,20 @@ variable "node_security_group_tags" {
   default     = {}
 }
 
+# TODO - at next breaking change, make 169.254.169.123/32 the default
+variable "node_security_group_ntp_ipv4_cidr_block" {
+  description = "IPv4 CIDR block to allow NTP egress. Default is public IP space, but [Amazon Time Sync Service](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/set-time.html) can be used as well with `[\"169.254.169.123/32\"]`"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+# TODO - at next breaking change, make fd00:ec2::123/128 the default
+variable "node_security_group_ntp_ipv6_cidr_block" {
+  description = "IPv4 CIDR block to allow NTP egress. Default is public IP space, but [Amazon Time Sync Service](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/set-time.html) can be used as well with `[\"fd00:ec2::123/128\"]`"
+  type        = list(string)
+  default     = ["::/0"]
+}
+
 ################################################################################
 # IRSA
 ################################################################################

commit 98a839f35dd08d82e260952715ee6aee99bbade8

Fix: Addresses typo introduced in #2125
The example extends the Security Group NTP IPV4 CIDR block, however,
it's meant to update the IPV6 CIDR block

examples/eks_managed_node_group/main.tf
@@ -92,7 +92,7 @@ module "eks" {
   }
 
   # Extend node-to-node security group rules
-  node_security_group_ntp_ipv4_cidr_block = ["fd00:ec2::123/128"]
+  node_security_group_ntp_ipv6_cidr_block = ["fd00:ec2::123/128"]
   node_security_group_additional_rules = {
     ingress_self_all = {
       description = "Node to node all ports/protocols"