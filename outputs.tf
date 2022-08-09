8e6315c5d141e6d4b2546523193da2d6bb63ec9e
Bryant Biggs <bryantbiggs@gmail.com>
2022-08-09 14:05:44 -0400

    fix: Remove empty `""` from node group names output when node group creation is disabled

--- outputs.tf
+++ outputs.tf
@@ -179,7 +179,7 @@ output "eks_managed_node_groups" {
 
 output "eks_managed_node_groups_autoscaling_group_names" {
   description = "List of the autoscaling group names created by EKS managed node groups"
-  value       = flatten([for group in module.eks_managed_node_group : group.node_group_autoscaling_group_names])
+  value       = compact(flatten([for group in module.eks_managed_node_group : group.node_group_autoscaling_group_names]))
 }
 
 ################################################################################
@@ -193,7 +193,7 @@ output "self_managed_node_groups" {
 
 output "self_managed_node_groups_autoscaling_group_names" {
   description = "List of the autoscaling group names created by self-managed node groups"
-  value       = [for group in module.self_managed_node_group : group.autoscaling_group_name]
+  value       = compact([for group in module.self_managed_node_group : group.autoscaling_group_name])
 }
 
 ################################################################################
