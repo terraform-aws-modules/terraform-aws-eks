9f15565edd315b2f24a4399a77ee2b2057962bca
Fernando Miguel <168366+FernandoMiguel@users.noreply.github.com>
2022-08-16 19:47:39 +0100

    add extra cluster_enabled_log_types
    sync up from blueprints
    https://github.com/aws-ia/terraform-aws-eks-blueprints/blob/b9d7acce/variables.tf#L177

--- variables.tf
+++ variables.tf
@@ -35,7 +35,7 @@ variable "cluster_version" {
 variable "cluster_enabled_log_types" {
   description = "A list of the desired control plane logs to enable. For more information, see Amazon EKS Control Plane Logging documentation (https://docs.aws.amazon.com/eks/latest/userguide/control-plane-logs.html)"
   type        = list(string)
-  default     = ["audit", "api", "authenticator"]
+  default     = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
 }
 
 variable "cluster_additional_security_group_ids" {
