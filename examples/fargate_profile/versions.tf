28ccecefe22d81a3a7febbbc3efc17c6590f88e1
Bryant Biggs <bryantbiggs@gmail.com>
2022-10-14 09:16:57 -0400

    fix: Disable creation of cluster security group rules that map to node security group when `create_node_security_group` = `false` (#2274)
    * fix: Disable creation of cluster security group rules that map to node security group when `create_node_security_group` = `false`
    
    * feat: Update Fargate example to run only Fargate and show disabling of both cluster and node security groups
    
    * fix: Ensure CoreDNS changes are made ahead of install

--- examples/fargate_profile/versions.tf
+++ examples/fargate_profile/versions.tf
@@ -6,9 +6,13 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 3.72"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.10"
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.7"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0"
     }
   }
 }
