87ced51b7e5a222b201d18f1005c9632d8ae6997
Anton Babenko <anton@antonbabenko.com>
2022-10-25 13:51:07 +0200

    Fixed IAM policy cluster_encryption when create_kms_key is false

--- main.tf
+++ main.tf
@@ -347,7 +347,7 @@ resource "aws_iam_policy" "cluster_encryption" {
           "kms:DescribeKey",
         ]
         Effect   = "Allow"
-        Resource = var.create_kms_key ? [module.kms.key_arn] : [for config in var.cluster_encryption_config : config.provider_key_arn]
+        Resource = var.create_kms_key ? module.kms.key_arn : var.cluster_encryption_config.provider_key_arn
       },
     ]
   })
