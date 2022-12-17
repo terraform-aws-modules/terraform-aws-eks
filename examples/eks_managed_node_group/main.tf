ece82472a62d7674b10261701bc1ec697988638f
Marvin Wichmann <marvin@fam-wichmann.de>
2022-12-17 21:06:12 +0100

    chore: Fix typo for vpc-cni addon example (#2349)
    Fix typo for vpc-cni addon
    
    All environment variables have to be strings

--- examples/eks_managed_node_group/main.tf
+++ examples/eks_managed_node_group/main.tf
@@ -67,8 +67,8 @@ module "eks" {
       configuration_values = jsonencode({
         env = {
           # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
-          ENABLE_PREFIX_DELEGATION = true
-          WARM_PREFIX_TARGET       = 1
+          ENABLE_PREFIX_DELEGATION = "true"
+          WARM_PREFIX_TARGET       = "1"
         }
       })
     }
