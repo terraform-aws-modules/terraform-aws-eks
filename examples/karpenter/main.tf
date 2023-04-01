322ab72682ba77491586211956b6d36eaf4013cb
유용환 <33824408+yoosful@users.noreply.github.com>
2023-04-01 20:38:10 +0900

    fix comment

--- examples/karpenter/main.tf
+++ examples/karpenter/main.tf
@@ -84,7 +84,7 @@ module "eks" {
     coredns = {
       configuration_values = jsonencode({
         computeType = "Fargate"
-        # Ensure that the we fully utilize the minimum amount of resources that are supplied by
+        # Ensure that we fully utilize the minimum amount of resources that are supplied by
         # Fargate https://docs.aws.amazon.com/eks/latest/userguide/fargate-pod-configuration.html
         # Fargate adds 256 MB to each pod's memory reservation for the required Kubernetes
         # components (kubelet, kube-proxy, and containerd). Fargate rounds up to the following
