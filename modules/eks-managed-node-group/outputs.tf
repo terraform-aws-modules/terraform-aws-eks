8f9ef96e1fb07864681f1071b6bb54cd02a41c7f
Vinod <vinod.dampuru@thoughtworks.com>
2023-05-12 23:18:50 +0800

    Adding new output variable for `user_data` in `managed_node_groups`

--- modules/eks-managed-node-group/outputs.tf
+++ modules/eks-managed-node-group/outputs.tf
@@ -88,3 +88,11 @@ output "iam_role_unique_id" {
   description = "Stable and unique string identifying the IAM role"
   value       = try(aws_iam_role.this[0].unique_id, null)
 }
+
+################################################################################
+# Additional
+################################################################################
+output "user_data" {
+  description = "Base64 encoded user data"
+  value       = try(module.user_data.user_data, null)
+}
