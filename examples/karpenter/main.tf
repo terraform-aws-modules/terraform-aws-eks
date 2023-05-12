aaad9324c6848ee7be9692abdf83f8d2e5ff5ae3
Kevin Jamieson <kevin.jamieson@xilis.net>
2023-05-12 23:21:29 +0000

    Changed the region, including an example deployment yaml which exhibits the problem

--- examples/karpenter/main.tf
+++ examples/karpenter/main.tf
@@ -55,7 +55,7 @@ data "aws_ecrpublic_authorization_token" "token" {
 locals {
   name            = "ex-${replace(basename(path.cwd), "_", "-")}"
   cluster_version = "1.24"
-  region          = "eu-west-1"
+  region          = "eu-central-1"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
