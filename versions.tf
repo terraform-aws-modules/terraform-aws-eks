2dea8551946b24e5c527656972a8f52e3f946371
Linus Seino <linus.seino@betterworks.com>
2023-08-29 11:49:34 -0600

    setting karpenter up with roles && auth config

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12.9"
 
   required_providers {
-    aws        = ">= 3.19.0"
+    aws        = ">= 4.19.0"
     local      = ">= 1.2"
     null       = ">= 2.1"
     template   = ">= 2.1"
