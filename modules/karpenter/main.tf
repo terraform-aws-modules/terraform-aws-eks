c36c8dc825aa09e2ded20ff675905aa8857853cf
Bryant Biggs <bryantbiggs@gmail.com>
2023-11-10 08:59:00 -0500

    fix: Remove additional conditional on Karpenter instance profile creation to support upgrading (#2812)

--- modules/karpenter/main.tf
+++ modules/karpenter/main.tf
@@ -386,7 +386,7 @@ locals {
 }
 
 resource "aws_iam_instance_profile" "this" {
-  count = var.create && var.create_instance_profile && !var.enable_karpenter_instance_profile_creation ? 1 : 0
+  count = var.create && var.create_instance_profile ? 1 : 0
 
   name        = var.iam_role_use_name_prefix ? null : local.iam_role_name
   name_prefix = var.iam_role_use_name_prefix ? "${local.iam_role_name}-" : null
