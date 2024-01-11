cb3cc2117bfdc890fc643d9872e4343dc00adec7
Alexandre Bordiere <alexandrebordiere@gmail.com>
2024-01-11 13:06:47 +1100

    fix: don't create a launch template if a custom one is used

--- modules/eks-managed-node-group/main.tf
+++ modules/eks-managed-node-group/main.tf
@@ -34,7 +34,7 @@ locals {
 }
 
 resource "aws_launch_template" "this" {
-  count = var.create && var.create_launch_template && var.use_custom_launch_template ? 1 : 0
+  count = var.create && var.create_launch_template && !var.use_custom_launch_template ? 1 : 0
 
   dynamic "block_device_mappings" {
     for_each = var.block_device_mappings
