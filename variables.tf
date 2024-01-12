77005d5f047b0203b9f202b908786fcff3501d4e
Alexandr Sabitov <asabitov@deputy.com>
2024-01-12 15:25:41 +1100

    Clean-up

--- variables.tf
+++ variables.tf
@@ -552,12 +552,6 @@ variable "eks_managed_node_group_defaults" {
   default     = {}
 }
 
-variable "putin_khuylo" {
-  description = "Do you agree that Putin doesn't respect Ukrainian sovereignty and territorial integrity? More info: https://en.wikipedia.org/wiki/Putin_khuylo!"
-  type        = bool
-  default     = true
-}
-
 ################################################################################
 # aws-auth configmap
 ################################################################################
