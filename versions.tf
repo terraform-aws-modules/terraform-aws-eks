d5a385567d9690d03462083fa96e45076b367b82
dtscssap <146767262+dtscssap@users.noreply.github.com>
2024-01-26 20:37:32 +0000

    require aws provider >= 5.34

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.57"
+      version = ">= 5.34"
     }
     tls = {
       source  = "hashicorp/tls"
