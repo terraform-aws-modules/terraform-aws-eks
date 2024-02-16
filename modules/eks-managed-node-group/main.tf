4752ff219880453dd9665e8cced86572644db0e3
abhashsolanki18 <157798417+abhashsolanki18@users.noreply.github.com>
2024-02-16 13:55:10 +0530

    Update main.tf
    name_prefix errors resolve

--- modules/eks-managed-node-group/main.tf
+++ modules/eks-managed-node-group/main.tf
@@ -35,6 +35,9 @@ locals {
 
 resource "aws_launch_template" "this" {
   count = var.create && var.create_launch_template && var.use_custom_launch_template ? 1 : 0
+ 
+  name        = var.launch_template_use_name_prefix ? null : local.launch_template_name
+  name_prefix = var.launch_template_use_name_prefix ? "${local.launch_template_name}-" : null
 
   dynamic "block_device_mappings" {
     for_each = var.block_device_mappings
@@ -211,8 +214,7 @@ resource "aws_launch_template" "this" {
     }
   }
 
-  name        = var.launch_template_use_name_prefix ? null : local.launch_template_name
-  name_prefix = var.launch_template_use_name_prefix ? "${local.launch_template_name}-" : null
+  
 
   dynamic "network_interfaces" {
     for_each = var.network_interfaces
