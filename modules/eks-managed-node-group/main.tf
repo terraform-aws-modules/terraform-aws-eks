d92ab0bc8b97c4933a26b48594f3b7b427613575
Josephuss <32011432+Josephuss@users.noreply.github.com>
2024-05-20 18:00:38 +0800

    Update main.tf

--- modules/eks-managed-node-group/main.tf
+++ modules/eks-managed-node-group/main.tf
@@ -464,7 +464,7 @@ resource "aws_eks_node_group" "this" {
     { Name = var.name }
   )
   depends_on = [
-    var.create_placement_group ? aws_placement_group.this : null
+    aws_placement_group.this
   ]
 }
 
