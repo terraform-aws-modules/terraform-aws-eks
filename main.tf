98967573f5ca9c24bba313c2ce33c8f87eb49b60
Bryant Biggs <bryantbiggs@gmail.com>
2024-06-03 17:30:38 -0400

    fix: Correct syntax for correctly ignoring `bootstrap_cluster_creator_admin_permissions` and not all of `access_config`

--- main.tf
+++ main.tf
@@ -105,7 +105,7 @@ resource "aws_eks_cluster" "this" {
 
   lifecycle {
     ignore_changes = [
-      access_config["bootstrap_cluster_creator_admin_permissions"]
+      access_config[0].bootstrap_cluster_creator_admin_permissions
     ]
   }
 }
