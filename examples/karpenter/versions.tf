ae25798f8c0a5dc7486ee5f567624842ed02e62f
William Escobar Parra <william.escobar.parra@itv.com>
2024-08-20 16:01:37 +0100

    move to alekc fork of kubectl provider

--- examples/karpenter/versions.tf
+++ examples/karpenter/versions.tf
@@ -15,8 +15,8 @@ terraform {
       version = ">= 2.7"
     }
     kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14"
+      source  = "alekc/kubectl"
+      version = ">= 2.0.4"
     }
     null = {
       source  = "hashicorp/null"
