eae2d3c2d357bd24eb69a298abab95d648cdb092
Dennis Ameling <dennis@dennisameling.com>
2025-07-07 12:43:16 +0200

    Bump AWS provider to v6

--- examples/karpenter/versions.tf
+++ examples/karpenter/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.95, < 6.0.0"
+      version = ">= 6.0.0"
     }
     helm = {
       source  = "hashicorp/helm"
