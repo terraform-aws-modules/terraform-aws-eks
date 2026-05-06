0f40e07acfb81a317d6f24c464f30aa633e70ff2
Demitri Morgan <demitri@pagerduty.com>
2026-05-05 18:08:20 -0600

    fix: endpoint disabled = omit public CIDRs
    When the public endpoint is disabled, i.e. for security, the CIDR prefixes are still delivered to the AWS API.
    
    This results in two security group rules for random chosen ports getting created in the default security group for the cluster that permit traffic from any address (0.0.0.0/0).
    
    While there might be no service running on those ports, any public entity could brute-force search for the open security group rules, and then be able to send arbitrary network traffic to the EKS network stack.
    
    Because the argument is optional*, we can omit it when the public interface is disabled, so that EKS will not automatically create the unnecessary security group rules.
    
    * See:
    https://docs.aws.amazon.com/eks/latest/APIReference/API_VpcConfigRequest.html#AmazonEKS-Type-VpcConfigRequest-publicAccessCidrs

--- main.tf
+++ main.tf
@@ -75,12 +75,28 @@ resource "aws_eks_cluster" "this" {
     }
   }
 
-  vpc_config {
-    security_group_ids      = compact(distinct(concat(var.additional_security_group_ids, [local.security_group_id])))
-    subnet_ids              = coalescelist(var.control_plane_subnet_ids, var.subnet_ids)
-    endpoint_private_access = var.endpoint_private_access
-    endpoint_public_access  = var.endpoint_public_access
-    public_access_cidrs     = var.endpoint_public_access_cidrs
+  dynamic "vpc_config" {
+    for_each = var.endpoint_public_access ? [] : ["without-public_access_cidrs"]
+
+    content {
+      security_group_ids      = compact(distinct(concat(var.additional_security_group_ids, [local.security_group_id])))
+      subnet_ids              = coalescelist(var.control_plane_subnet_ids, var.subnet_ids)
+      endpoint_private_access = var.endpoint_private_access
+      endpoint_public_access  = var.endpoint_public_access
+      public_access_cidrs     = var.endpoint_public_access_cidrs
+    }
+  }
+
+  dynamic "vpc_config" {
+    for_each = var.endpoint_public_access ? ["with-public_access_cidrs"] : []
+
+    content {
+      security_group_ids      = compact(distinct(concat(var.additional_security_group_ids, [local.security_group_id])))
+      subnet_ids              = coalescelist(var.control_plane_subnet_ids, var.subnet_ids)
+      endpoint_private_access = var.endpoint_private_access
+      endpoint_public_access  = var.endpoint_public_access
+      public_access_cidrs     = var.endpoint_public_access_cidrs
+    }
   }
 
   dynamic "kubernetes_network_config" {
